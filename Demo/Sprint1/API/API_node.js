81a864c597a2b172738a008bdec08f08384f4a34
everdun <everdun@sqli.com>
2016-08-18 10:14:47 +0200

    erreur dans Demo/Sprint1/API/API_node.js

--- Demo/Sprint1/API/API_node.js
+++ Demo/Sprint1/API/API_node.js
@@ -1,165 +1,130 @@
 var express = require('express');
-var fs = require('fs');
-var beacon = require('eddystone-beacon');
-var localIp = require('my-local-ip')();
-var solc = require('solc');
 var web3 = require('./utils/web3IPCExtension').web3;
 var Q = require('q');
-var gpio = require('rpi-gpio');
+var http = require('http');
+var Promise = require('promise');
 
 
 
 // params
 var compiled, contract;
-var source = "";
 
 
 // Solidity Contract
 web3.setProvider(new web3.providers.HttpProvider('http://0.0.0.0:8547'));
 var account = web3.eth.accounts[0];
 
-var createdAtBlock = web3.eth.blockNumber;
-
-// listen for created log/event
-var Created = web3.eth.filter({
-    topics: [null, '0x16bd7d60bc08217d2e78d09658610a9eb6de22df8b587fdca9e980fafc4ecfcc'],
-    fromBlock: createdAtBlock,
-    toBlock: 'latest'
-});
-
-Created.watch(function(error, log) {
-    if (!error) {
-        console.log('Contract created on ' + log.address);
-
-        contract.address = log.address;
-
-        // remove filter
-        Created.stopWatching();
-
-        // watch for the last next 12 blocks if the code is still at the address
-        var filter = web3.eth.filter('latest');
-        filter.watch(function(e, blockHash) {
-            if (!e) {
-                var block = web3.eth.getBlock(blockHash);
-
-                // check if contract stille exists, if show error to the user
-                if ((block.number - createdAtBlock) < 12 &&
-                    web3.eth.getCode(contract.address).length <= 2) {
-                    console.log('The contract is gone!');
-                } else if (block.number - createdAtBlock > 12) {
-                    filter.stopWatching();
-                }
-            }
-        });
-    }
-});
-
-
-addPeer().then(function() {
-    console.log('addPeer OK');
-    readSource().then(function() {
-        console.log('Source OK');
-        unlockAccount().then(function() {
-            console.log('Unlocked');
-            compiled = solc.compile(source, 1);
-            compiled.then(initContract());
-        })
+var optionsHttp = {
+    host: "10.33.44.182",
+    port: 8080,
+};
+var data;
+http.get(optionsHttp, function(resp) {
+    resp.on('data', function(chunk) {
+        data = JSON.parse(chunk);
     })
 })
 
+// http server on 8080 port
+console.log('server OK');
+// Server sur port 8080
+var app = express();
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-// add two demo nodes (with promise)
-function addPeer() {
-    var deferred = Q.defer();
-    var bool = web3.admin.addPeer("enode://d9b86a2036cb10c0ae7bd299d8218eae27e7006df67208aa375f4341fe1a5610e1616b516c83d849c302a42200e6b3bde688fc39d456668613a5d8345c239d39@10.33.44.155:30301", "enode://4fd325fd92a22ed7ef4cfa40d9645761ff4bac064d31c8a5f22dd3e4684d3e6ba006dc03907de01b4780bb8380b1e148c45a1d196be583f3607d41ede95daddd@10.33.44.155:30302");
-    if (bool) {
-        deferred.resolve();
-    }
-    return deferred.promise;
-}
+app.get('/scan', function(req, res) {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end(JSON.stringify(data));
+});
 
-// read the contract file et write it inline (with promise)
-function readSource() {
-    var deferred = Q.defer();
-    var lineReader = require('readline').createInterface({
-        input: fs.createReadStream('./contract/contract.sol')
-    });
-    lineReader.on('line', function(line) {
-        source += line;
-    });
-    lineReader.on('close', function() {
-        deferred.resolve();
-    });
-    return deferred.promise;
-}
+app.get('/louer', function(req, res) {
+    var cont = req.param('contract');
+    //  contract = joinContract(cont.abi, cont.address);
+    contract = web3.eth.contract(JSON.parse(cont).abi).at(JSON.parse(cont).address);
+    web3.personal.unlockAccount(account, 'noeud2', 360);
+    Promise.denodeify(contract.RentMe.sendTransaction)({
+        from: account
+    }).then(function() {
+        console.log("Contract lié.");
 
-// unlock account for set transaction/new contract instance (with promise)
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+
+app.get('/goto', function(req, res) {
+    var x = req.param('x');
+    var y = req.param('y');
+    goTo = contract.GoTo.call(x, y);
+    var price = goTo[1].c[0];
+    console.log(price);
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end(JSON.stringify(price));
+});
+
+app.get('/goo', function(req, res) {
+    var price = req.param('price');
+    contract.StartRent.sendTransaction({
+        from: account,
+        value: price
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+app.get('/annuler', function(req, res) {
+    web3.personal.unlockAccount(account, 'noeud2', 60);
+    contract.StopRent.sendTransaction({
+        from: account
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+app.get('/img', function(req, res) {
+    var img = fs.readFileSync('./image/*.jpg')
+    res.writeHead(200, {
+        "Content-Type": "image/jpeg"
+    });
+    res.end(img);
+});
+
+
+app.listen(8088);
+
+app.on('connection', function() {
+    console.log('Availabled server');
+});
+
+
 function unlockAccount() {
     var deferred = Q.defer();
-    var bool = web3.personal.unlockAccount(account, 'raspberry', 360);
+    var bool = web3.personal.unlockAccount(account, 'noeud2', 360);
     if (bool) {
         deferred.resolve();
+        console.log("unlock account");
     }
     return deferred.promise;
 }
 
-// create a new contract instance (with promise)
-function initContract() {
-    console.log('Compiled');
-    var deferred = Q.defer();
-    for (var contractName in compiled.contracts) {
-        console.log('account : ', account);
-        contract = web3.eth.contract(JSON.parse(compiled.contracts[contractName].interface)).new('0x16bd7d60bc08217d2e78d09658610a9eb6de22df8b587fdca9e980fafc4ecfcc', {
-            from: web3.eth.accounts[0],
-            data: '0x' + compiled.contracts[contractName].bytecode,
-            gas: 250000
-        });
-        console.log(contract);
-    }
-  console.log('Contract OK');
-    server();
-    deferred.resolve();
-    return deferred.promise;
+function joinContract(abi, address) {
+    console.log(abi);
+    console.log(address);
+    cont = web3.eth.contract(abi).at(address);
+    console.log("Added contract");
+    return cont;
 }
-
-// http server on 8080 port
-function server() {
-    console.log('server OK');
-    // Server sur port 8080
-    var app = express();
-    app.get('/', function(req, res) {
-        res.writeHead(200, {
-            "Content-Type": "application/json"
-        });
-        contractRPI = {
-            abi: contract.abi,
-            address: contract.address
-        };
-        json = JSON.stringify({
-            name: "choupette",
-            manufacturer: "Volkswagen",
-            model: "Coccinelle",
-            contract: contractRPI
-        });
-        res.end(json);
-    });
-
-    app.get('/img', function(req, res) {
-        var img = fs.readFileSync('./image/*.jpg')
-        res.writeHead(200, {
-            "Content-Type": "image/jpeg"
-        });
-        res.end(img);
-    });
-
-    app.use(function(req, res, next) {
-        res.setHeader('Content-Type', 'text/plain');
-        res.status(404).send('Page introuvable')
-    });
-
-    app.listen(8080);
-
-    app.on('connection', function() {
-        console.log('Availabled server');
-    });
-};
