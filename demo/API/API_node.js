8a7983cd75ea2f1106caa4e18379471cd85f6d21
everdun <everdun@sqli.com>
2016-09-14 14:56:48 +0200

    remise en place de demo/

---
+++ demo/API/API_node.js
@@ -0,0 +1,182 @@
+var express = require('express');
+var web3 = require('./utils/web3IPCExtension').web3;
+var Q = require('q');
+var http = require('http');
+var Promise = require('promise');
+
+
+
+// params
+var compiled, contract;
+
+
+// Solidity Contract
+web3.setProvider(new web3.providers.HttpProvider('http://0.0.0.0:8547'));
+//web3.setProvider(new web3.providers.HttpProvider('http://10.33.44.182:8547'));
+var account = web3.eth.accounts[0];
+var pwd = 'noeud2';
+//var pwd = 'raspberry';
+
+
+var optionsHttp = {
+    host: "10.33.44.182",
+    port: 8080,
+};
+var data;
+http.get(optionsHttp, function(resp) {
+    resp.on('data', function(chunk) {
+        data = JSON.parse(chunk);
+    })
+})
+
+// http server on 8080 port
+console.log('server OK');
+// Server sur port 8080
+var app = express();
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/scan', function(req, res) {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end(JSON.stringify(data));
+});
+
+app.get('/louer', function(req, res) {
+    var cont = req.query.contract;
+    //  contract = joinContract(cont.abi, cont.address);
+    contract = web3.eth.contract(JSON.parse(cont).abi).at(JSON.parse(cont).address);
+    web3.personal.unlockAccount(account, pwd, 360);
+    Promise.denodeify(contract.RentMe.sendTransaction)({
+        from: account
+    }).then(function() {
+        console.log("Contract lié.");
+
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+
+app.get('/goto', function(req, res) {
+    var x = req.query.x;
+    var y = req.query.y;
+    var price;
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    contract.GoTo.sendTransaction(x, y, {
+        from: account
+    }, function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        txhash = result;
+        filter = web3.eth.filter('latest');
+        filter.watch(function(error, result) {
+            var receipt = web3.eth.getTransactionReceipt(txhash);
+            if (receipt && receipt.transactionHash == txhash) {
+                filter.stopWatching();
+                price = contract.GetPrice.call();
+                if (price == 0) {
+                    price = "error";
+                }
+                res.end(JSON.stringify(price))
+            }
+        })
+    });
+});
+
+app.get('/goo', function(req, res) {
+    var price = req.query.price;
+    web3.personal.unlockAccount(account, pwd, 60);
+    contract.StartRent.sendTransaction({
+        from: account,
+        value: price
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+app.get('/annuler', function(req, res) {
+    web3.personal.unlockAccount(account, pwd, 60);
+    contract.StopRent.sendTransaction({
+        from: account
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+app.get('/valide', function(req, res) {
+    web3.personal.unlockAccount(account, pwd, 60);
+    contract.ValidateTravel.sendTransaction({
+        from: account
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end();
+});
+
+app.get('/carvalide', function(req, res) {
+    res.writeHead(200, {
+        'Connection': 'keep-alive',
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache'
+    });
+
+    // Watch events
+    contract.OnStateChanged().watch(function(error, result) {
+        switch (result.args.state.c[0]) {
+            case 0:
+                //console.log('vert');
+                break;
+            case 1:
+                //console.log('orange');
+                break;
+            case 2:
+                //console.log('rouge');
+                break;
+            case 3:
+                //console.log('bleu');
+                res.write('data: ' + JSON.stringify({
+                    msg: "validateByCar"
+                }) + '\n\n');
+                break;
+        }
+    })
+});
+
+app.listen(8088);
+
+
+
+function unlockAccount() {
+    var deferred = Q.defer();
+    var bool = web3.personal.unlockAccount(account, pwd, 360);
+    if (bool) {
+        deferred.resolve();
+        console.log("unlock account");
+    }
+    return deferred.promise;
+}
+
+function joinContract(abi, address) {
+    console.log(abi);
+    console.log(address);
+    cont = web3.eth.contract(abi).at(address);
+    console.log("Added contract");
+    return cont;
+}
---
+++ demo/app.js
@@ -0,0 +1,105 @@
+var contract, data, price;
+var validate = 0;
+var optionsHttp = {
+    host: "10.33.44.182",
+    port: 8080
+};
+
+
+$('#scan').on('click', function() {
+    $.get(
+        "http://localhost:8088/scan",
+        function(data) {
+            console.log(typeof data.contract.abi);
+            console.log(typeof data.contract.address);
+            document.getElementById('nomVoiture').innerHTML = data.name;
+            document.getElementById('2').style.display = "inline";
+            $('#scan').prop('disabled', true);
+            contract = data.contract;
+        }
+    );
+})
+
+$('#louer').on('click', function() {
+    $.get(
+        "http://localhost:8088/louer", {
+            contract: JSON.stringify(contract)
+        }).done(
+        function() {
+            document.getElementById('3').style.display = "inline";
+            $('#louer').prop('disabled', true);
+            var source = new EventSource("http://localhost:8088/carvalide");
+            source.onmessage = function(e) {
+                var jsonData = JSON.parse(e.data);
+                var valideChoupette = document.getElementById('valideChoupette');
+                valideChoupette.innerHTML = "Trajet validé par Choupette";
+                valideChoupette.className = "btn btn-success";
+                ++validate;
+                console.log(validate);
+                if (validate == 2)
+                    document.getElementById('aurevoir').style.display = "inline";
+
+            };
+        }
+    );
+});
+
+function goto(pos) {
+    var xpos = document.pos.Xpos.value;
+    var ypos = document.pos.Ypos.value;
+    $.get(
+        "http://localhost:8088/goto", {
+            x: xpos,
+            y: ypos
+        }).done(
+        function(data) {
+          if (data == "error") {
+            document.getElementById('error').style.display = "inline";
+          } else {
+            document.getElementById('prix').innerHTML = data;
+            price = data;
+            document.getElementById('4').style.display = "inline";
+            document.getElementById('error').style.display = "none";
+            console.log(data);
+            $('#go').prop('disabled', true);
+          }
+        }
+    );
+}
+
+$('#terminer').on('click', function() {
+    $.get(
+        "http://localhost:8088/goo", {
+            price: price
+        }).done(
+        function() {
+            $('.btn').prop('disabled', true);
+            document.getElementById('bonvoyage').style.display = "inline";
+            $('.valide').prop('disabled', false);
+
+        }
+    );
+});
+
+$('.annuler').on('click', function() {
+    $.get(
+        "http://localhost:8088/annuler").done(
+        function() {
+            $('.btn').prop('disabled', true);
+            document.getElementById('aurevoir').style.display = "inline";
+        }
+    );
+});
+
+$('#valide').on('click', function() {
+    $.get(
+        "http://localhost:8088/valide").done(
+        function() {
+            ++validate;
+            console.log(validate);
+            $('.btn').prop('disabled', true);
+            if (validate == 2)
+                document.getElementById('aurevoir').style.display = "inline";
+        }
+    );
+});
